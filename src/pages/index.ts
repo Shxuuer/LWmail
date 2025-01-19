8d70f1685224f67d24a49bfceb75731c7f5a86f0
Shxuuer <a2711492759@163.com>
2025-01-19 23:33:13 +0800

    tmp: add ui

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 const info: HTMLElement = document.getElementById('info')!;
 
-window.mail.getMailBoxes().then((boxes: any) => {
-  info.innerText = JSON.stringify(boxes);
-});
+// window.mail.getMailBoxes().then((boxes: any) => {
+//   info.innerText = JSON.stringify(boxes);
+// });
