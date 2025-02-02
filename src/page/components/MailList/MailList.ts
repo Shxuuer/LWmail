a704b47447865c14a5725425eeb9cbbd83f51f50
shxuuer <shxuuer@outlook.com>
2025-02-02 16:15:50 +0800

    feat: rewrite ui engine with jquery and scss

--- src/page/components/MailList/MailList.ts
+++ src/page/components/MailList/MailList.ts
@@ -2,191 +2,152 @@ import "./MailList.scss";
 import dayjs from "dayjs";
 import $ from "jquery";
 
-export let selectedMail: string | boolean = false;
+let selectedMail: JQuery<HTMLElement> = null;
 
-export function setSelectMail(value: string | boolean) {
+// jquery elements of mail list page
+let accounts: JQuery<HTMLElement> = null;
+let mails: JQuery<HTMLElement> = null;
+
+export function setSelectMail(value: JQuery<HTMLElement>) {
+  if (selectedMail === value) return;
+  if (selectedMail) selectedMail.css("background-color", "transparent");
   selectedMail = value;
+  if (selectedMail) selectedMail.css("background-color", "#e0e5ff");
+}
+
+export function getSelectMail(): JQuery<HTMLElement> {
+  return selectedMail;
 }
 
 /**
- * create one mail preview
- * @param mailInfo mail information
- * @returns HTMLElement mail preview
+ * Create mail list frame
+ * @returns {JQuery<HTMLElement>} mail list frame
  */
-function createOneMailPreview(mailInfo: Message) {
-  const mail = document.createElement("div");
-  mail.className = "left-bar-mails-content-each";
-  const mailText = document.createElement("div");
-  mailText.className = "left-bar-mails-content-each-text";
-  const mailTextFrom = document.createElement("div");
-  mailTextFrom.className = "left-bar-mails-content-each-text-from";
-  mailTextFrom.innerText = mailInfo.from.name;
-  const mailTextTitle = document.createElement("div");
-  mailTextTitle.className = "left-bar-mails-content-each-text-title";
-  mailTextTitle.innerText = mailInfo.subject;
-  const mailTextTime = document.createElement("div");
-  mailTextTime.className = "left-bar-mails-content-each-text-time";
-  const edate = dayjs(mailInfo.date);
-  mailTextTime.innerText = edate.format("ddd MM/DD");
-  mailText.appendChild(mailTextFrom);
-  mailText.appendChild(mailTextTitle);
-  mailText.appendChild(mailTextTime);
-  mail.appendChild(mailText);
-  mail.addEventListener("click", () => {
-    const mailDetail = document.getElementById("mail-detail") as HTMLElement;
-    mailDetail.style.display = "block";
-    if (mailInfo.source === undefined) {
-      window.mail.getHtmlByUid(mailInfo.to.address, "INBOX", mailInfo.uid);
-    }
-    mailDetail.innerHTML = mailInfo.source;
-  });
-  return mail;
+export function createMailList(): JQuery<HTMLElement> {
+  accounts = $("<div>").attr("id", "accounts");
+  mails = $("<div>")
+    .attr("id", "mails")
+    .append(
+      $("<div>")
+        .attr("id", "goback")
+        .append(
+          $("<img>")
+            .attr("src", require("../../../assets/img/off.svg"))
+            .attr("alt", "goback"),
+        )
+        .append($("<span>").text("Go back"))
+        .on("click", goBackToAccounts),
+    )
+    .append($("<div>").attr("id", "content"));
+
+  return $("<div>").attr("id", "left-bar").append(accounts).append(mails);
 }
 
 /**
- * create a mail box
- * @param box box name
- * @param accountIndex account index
- * @param boxIndex box index
- * @returns HTMLElement mail box
+ * left bar accounts: show accounts page
  */
-function createMailBox(box: Box) {
-  const mailBox = document.createElement("div");
-  mailBox.className = "mail-box";
-  const mailBoxName = document.createElement("span");
-  mailBoxName.className = "mail-box-name";
-  mailBoxName.innerText = box.boxPath;
-  mailBoxName.addEventListener("click", () => {
-    const accounts = document.getElementById(
-      "left-bar-accounts",
-    ) as HTMLElement;
-    const mails = document.getElementById("left-bar-mails") as HTMLElement;
-    const mailList = document.getElementById(
-      "left-bar-mails-content",
-    ) as HTMLElement;
-    mailList.innerHTML = "";
-    box.messages.forEach((mailInfo) => {
-      const mail = createOneMailPreview(mailInfo);
-      mailList.appendChild(mail);
-    });
-    accounts.style.transform = "translateX(-100%)";
-    mails.style.transform = "translateX(-100%)";
-  });
-  mailBox.appendChild(mailBoxName);
-  return mailBox;
+function goBackToAccounts() {
+  accounts.css("transform", "translateX(0)");
+  mails.css("transform", "translateX(0)");
 }
 
 /**
- * create mail boxes
- * @param boxes box names
- * @param accountIndex account index
- * @returns HTMLElement mail boxes
+ * left bar accounts: show mails page
+ * @param {Box} box - mails box
  */
-function createMailBoxes(boxes: Box[]) {
-  const mailBoxes = document.createElement("div");
-  mailBoxes.className = "mail-boxes";
-  boxes.forEach((box) => {
-    mailBoxes.appendChild(createMailBox(box));
-  });
-  return mailBoxes;
+function goForwardToMails(box: Box, accountAddr: string) {
+  let mailContent = mails.children("#content");
+  mailContent.html("");
+  box.messages.forEach((m) =>
+    mailContent.append(createMailThumbs(m, accountAddr, box.boxPath)),
+  );
+  accounts.css("transform", "translateX(-100%)");
+  mails.css("transform", "translateX(-100%)");
+}
+
+function previewMail(message: Message, accountAddr: string, boxPath: string) {
+  let htmlMessage: string = message.source;
+  if (htmlMessage === undefined)
+    htmlMessage = window.mail.getHtmlByUid(accountAddr, boxPath, message.uid);
+  $("#mail-detail").html(htmlMessage).css("display", "block");
 }
 
 /**
- * create one account element
- * @param mail account address
- * @param boxes account boxes
- * @returns HTMLElement mail element
+ * handle click mail address, open or close mail boxes
+ * @param event click event
  */
-export function createOneAccount(mail: string, boxes: Box[]) {
-  const oneMail = document.createElement("div");
-  oneMail.className = "one-mail";
-  const mailAddr = document.createElement("div");
-  mailAddr.className = "mail-addr";
-  mailAddr.id = mail;
-  mailAddr.setAttribute("open", "false");
-  const img = document.createElement("img");
-  img.src = require("../../../assets/img/off.svg");
-  const span = document.createElement("span");
-  span.innerText = mail;
-  mailAddr.appendChild(img);
-  mailAddr.appendChild(span);
-  oneMail.appendChild(mailAddr);
-  const mailBoxes = createMailBoxes(boxes);
-  mailBoxes.style.display = "none";
-  oneMail.appendChild(mailBoxes);
-
-  mailAddr.addEventListener("click", () => {
-    // select for delete
-    if (selectedMail !== mail) {
-      if (selectedMail) {
-        const lastMail = document.getElementById(selectedMail as string)!;
-        lastMail.style.backgroundColor = "transparent";
-      }
-      selectedMail = mail;
-      mailAddr.style.backgroundColor = "#e0e5ff";
-    }
-    if (mailAddr.getAttribute("open") === "false") {
-      mailAddr.setAttribute("open", "true");
-      img.style.transform = "rotate(90deg)";
-      mailBoxes.style.display = "flex";
-    } else {
-      mailAddr.setAttribute("open", "false");
-      img.style.transform = "rotate(0deg)";
-      mailBoxes.style.display = "none";
-    }
-  });
-  return oneMail;
-}
-
-/**
- * create a go back button
- */
-function createMailListGoBack() {
-  const leftBarGoBack = document.getElementById("left-bar-goback")!;
-  const img = document.createElement("img");
-  img.src = require("../../../assets/img/off.svg");
-  img.alt = "goback";
-  const span = document.createElement("span");
-  span.innerText = "Go back";
-  leftBarGoBack.appendChild(img);
-  leftBarGoBack.appendChild(span);
-  function goBack() {
-    const accounts = document.getElementById(
-      "left-bar-accounts",
-    ) as HTMLElement;
-    const mails = document.getElementById("left-bar-mails") as HTMLElement;
-    accounts.style.transform = "translateX(0)";
-    mails.style.transform = "translateX(0)";
+function handleClickMailAddr(event: JQuery.ClickEvent) {
+  const target = $(event.currentTarget);
+  setSelectMail(target);
+  if (target.attr("opened") === "false") {
+    target.attr("opened", "true");
+    target.children("img").css("transform", "rotate(90deg)");
+    target.next().css("display", "flex");
+  } else {
+    target.attr("opened", "false");
+    target.children("img").css("transform", "rotate(0deg)");
+    target.next().css("display", "none");
   }
-  leftBarGoBack.addEventListener("click", goBack);
 }
 
-function createLeftBarFrame() {
-  $("<div>").attr("id", "left-bar-accounts").addClass("left-bar-item");
-  $("<div>").attr("id", "left-bar-mails").addClass("left-bar-item");
-  $("<div>").attr("id", "left-bar-mails-content");
-  $("<div>").attr("id", "left-bar-goback");
-  return $("<div>").attr("id", "left-bar");
-  const leftBarAccounts = document.createElement("div");
-  leftBarAccounts.id = "left-bar-accounts";
-  leftBarAccounts.className = "left-bar-item";
-
-  const leftBarMails = document.createElement("div");
-  leftBarMails.id = "left-bar-mails";
-  leftBarMails.className = "left-bar-item";
-
-  const leftBarMailsContent = document.createElement("div");
-  leftBarMailsContent.id = "left-bar-mails-content";
-
-  const leftBarGoBack = document.createElement("div");
-  leftBarGoBack.id = "left-bar-goback";
-
-  const leftBar = document.getElementById("left-bar")!;
-  leftBar.appendChild(leftBarAccounts);
-  leftBar.appendChild(leftBarMails);
-  createMailListGoBack();
+/**
+ * Create mail list
+ * @param {AccountBox[]} message - account boxes
+ */
+function createMailThumbs(
+  message: Message,
+  accountAddr: string,
+  boxPath: string,
+): JQuery<HTMLElement> {
+  return $("<div>")
+    .on("click", () => previewMail(message, accountAddr, boxPath))
+    .append(
+      $("<div>")
+        .addClass("text")
+        .append($("<div>").addClass("text-from").text(message.from.name))
+        .append($("<div>").addClass("text-title").text(message.subject))
+        .append(
+          $("<div>")
+            .addClass("text-time")
+            .text(dayjs(message.date).format("ddd MM/DD")),
+        ),
+    );
 }
 
-export function createMailList() {
-  return createLeftBarFrame();
+/**
+ * Create one account
+ * @param accountBox - account box
+ */
+export function createOneAccount(accountBox: AccountBox) {
+  const boxesElements = accountBox.boxes.map((box) =>
+    $("<div>")
+      .text(box.boxPath)
+      .on("click", () => goForwardToMails(box, accountBox.accountAddr)),
+  );
+
+  const account = $("<div>")
+    .append(
+      $("<div>")
+        .addClass("mail-addr")
+        .attr("opened", "false")
+        .append($("<img>").attr("src", require("../../../assets/img/off.svg")))
+        .append($("<span>").text(accountBox.accountAddr))
+        .on("click", handleClickMailAddr),
+    )
+    .append(
+      $("<div>")
+        .addClass("mail-boxes")
+        .css("display", "none")
+        .append(boxesElements),
+    );
+  accounts.append(account);
+}
+
+/**
+ * Update mail list
+ * @param {AccountBox[]} accountBoxes - account box
+ */
+export function updateMailList(accountBoxes: AccountBox[]) {
+  accounts.html("");
+  accountBoxes.forEach((accountBox) => createOneAccount(accountBox));
 }
--- src/page/renderer.ts
+++ src/page/renderer.ts
@@ -1,25 +1,11 @@
 import "./index.scss";
 import "../assets/img/background.jpg";
 import $ from "jquery";
-// import {
-//   createOneAccount,
-//   createMailList,
-// } from "./components/MailList/MailList";
-
-// when mails update, update the left bar
-window.mail.onMailsUpdate((accountBoxes: AccountBox[]) => {
-  console.log(accountBoxes);
-  // const leftBar = document.getElementById("left-bar-accounts");
-  // if (leftBar) leftBar.innerHTML = "";
-  // accountBoxes.forEach((accountBox: AccountBox) => {
-  //   const inner = createOneAccount(accountBox.accountAddr, accountBox.boxes);
-  //   leftBar?.appendChild(inner);
-  // });
-});
 
 import { createHeader } from "./components/Header/Header";
-import { createMailList } from "./components/MailList/MailList";
+import { createMailList, updateMailList } from "./components/MailList/MailList";
 import { createPreview } from "./components/MailPreview/MailPreview";
+
 $("body").append(
   $("<div>")
     .attr("id", "container")
@@ -31,3 +17,9 @@ $("body").append(
         .append(createPreview()),
     ),
 );
+
+// when mails update, update the left bar
+window.mail.onMailsUpdate((accountBoxes: AccountBox[]) => {
+  console.log(accountBoxes);
+  updateMailList(accountBoxes);
+});
